d72db5a4a3d97c13d27fd184448717794e54ce5b
watanabe ryunosuke <46173232+berry8848@users.noreply.github.com>
2025-04-23 11:22:04 +0900

    Add files via upload

---
+++ CppTemplate/HelloWorld/build/CMakeFiles/main_exec.dir/src/main.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/main_exec.dir/src/main.cpp.o: \
+ /home/rdp_user/Documents/CppProject/HelloWorld/src/main.cpp \
+ /usr/include/stdc-predef.h \
+ /home/rdp_user/Documents/CppProject/HelloWorld/include/hello.h
---
+++ CppTemplate/HelloWorld/include/hello.h
@@ -0,0 +1,14 @@
+#ifndef HELLOWORLD_H
+#define HELLOWORLD_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void hello();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // HELLOWORLD_H
